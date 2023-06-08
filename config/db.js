7a694a8601ae338d1dde831dee3302095f684882
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2023-06-08 17:40:30 -0500

    Connecting to Mongo DB

---
+++ config/db.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const DB_URI = process.env.DB_URI;
+
+// Connection to MongoDB Atlas
+const dbConnect = () => {
+    mongoose.connect(DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then((db) => console.log("Mongodb is connected to", db.connection.host))
+    .catch((err) => console.log(`Error in the database connection ${err}`));
+}
+
+module.exports = dbConnect;
--- src/app.js
+++ src/app.js
@@ -3,18 +3,22 @@ const express = require("express");
 const app = express();
 const routes = require("../routes/index");
 const expressLayouts = require("express-ejs-layouts");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
 const bodyParser = require("body-parser");
+const dbConnect = require('../config/db');
 
 const PORT = 5000;
+
+// Connecting to DB
+dbConnect();
 
 // CORS (Cross-Origin Resource Sharing) supports and allows the server to respond to requests from different origins.
 app.use(cors());
 
 // Morgan sets up the Morgan middleware, which logs HTTP requests to the console. In this case, it uses the 'dev' format, which provides concise and colorful logs for development purposes.
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 // It configures the body-parser middleware to handle URL-encoded form data. It parses the request body and makes it accessible via req.body in subsequent middleware or route handlers. The { extended: true } option allows for complex objects to be encoded in the URL.
 app.use(bodyParser.urlencoded({ extended: true }));
