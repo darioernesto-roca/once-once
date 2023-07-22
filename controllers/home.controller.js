466e8a666f352f4730c140ec2b12cb8bedd53d0c
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2023-07-21 19:06:47 -0500

    Updating model, controller and views

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,4 +1,5 @@
 // home.controller.js
+/*
 const { readFeatures, readNews } = require('../helpers/readProducts');
 const formatPrice = require('../helpers/formatPrice');
 const { Product } = require('../models/product');
@@ -8,6 +9,36 @@ exports.index = async function (req, res) {
     const products = await Product.find().populate('category parentCategory');
     const formattedProducts = products.map(product => {
       console.log(product._id);
+      const formattedPrice = formatPrice(product.price);
+      return { ...product.toObject(), price: formattedPrice };
+    });
+
+    const news = readNews();
+    news.forEach(product => {
+      product.formattedPrice = formatPrice(product.price);
+    });
+
+    res.render('home', {
+      title: 'Once Once',
+      formattedProducts,
+      news,
+      req,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al obtener los productos');
+  }
+};
+*/
+
+const { readNews } = require('../helpers/readProducts');
+const formatPrice = require('../helpers/formatPrice');
+const { Product } = require('../models/product');
+
+exports.index = async function (req, res) {
+  try {
+    const products = await Product.find();
+    const formattedProducts = products.map(product => {
       const formattedPrice = formatPrice(product.price);
       return { ...product.toObject(), price: formattedPrice };
     });
--- models/product.js
+++ models/product.js
@@ -1,79 +1,20 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    size: [{
-        type: String,
-        default: '',
-        required: true,
-    }],
-    image: {
-        type: String,
-        default: '',
-        required: true,
-    },
-    images: [{
-        type: String,
-        required: true,
-    }],
-    colors: [{
-        type: String,
-        default: '',
-        required: true,
-    }],
-    availableColors: [{
-        type: String,
-        default: '',
-        required: true,
-    }],
-    price : {
-        type: Number,
-        default:0,
-        required: true,
-    },
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-        required:true
-    },
-    parentCategory: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-        required:true
-    },
-    countInStock: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 255
-    },
-    rating: {
-        type: Number,
-        default: 0,
-    },
-    numReviews: {
-        type: Number,
-        default: 0,
-    },
-    isFeatured: {
-        type: Boolean,
-        default: false,
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now,
-    },
-    cloudinary_id: {
-      type: String,
-    }
-})
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    size: [String],
+    countInStock: { type: Number, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    categories: [String],
+    subcategories: [String],
+    images: [String],
+    availableColors: [String],
+    colors: [String],
+    feature: { type: Boolean, default: false },
+    new: { type: Boolean, default: false },
+  });
 
 productSchema.virtual('id').get(function () {
     return this._id.toHexString();
