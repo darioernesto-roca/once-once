4ee4219f056905dcba53b9b4c0a2d5178f5f53a8
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2023-10-08 22:11:05 -0500

    Updating products, header, footer and sproduct page

--- src/app.js
+++ src/app.js
@@ -50,11 +50,3 @@ app.set("view engine", "ejs");
 
 // Set routes
 app.use(routes);
-
-//Epayco
-var epayco = require('epayco-sdk-node')({
-  apiKey: 'PUBLIC_KEY',
-  privateKey: 'PRIVATE_KEY',
-  lang: 'ES',
-  test: true
-})
---
+++ test.js
@@ -0,0 +1,22 @@
+exports.post = async function (req, res) {
+
+    const orderData = req.body;
+    const cartItems = orderData.products || []; // Use orderData.products instead of req.body.cartItems
+  
+    // Remove the 'products' field from the order data so it won't be saved in the database
+    delete orderData.products;
+  
+    // Assign the cartItems to the 'orderItems' property of the order
+    orderData.orderItems = cartItems.map((item) => {
+      return { product: item.productId, quantity: item.quantity };
+    });
+  
+    const newOrder = new Order(orderData);
+    newOrder.save()
+      .then(savedOrder => {
+        res.json(savedOrder);
+      })
+      .catch(_ => {
+        res.status(500).json({ error: 'Error al guardar la orden' });
+      });
+  };
